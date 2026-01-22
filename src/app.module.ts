4d5c75a2b92e90d82ea5bab1645d6dc7756c41f1
jgpeixoto <joaogabrielpeixoto2005@gmail.com>
2026-01-22 17:37:12 -0300

    refactor: clean up app module and fix migrate script

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { FileModule } from './files/file.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [MoviesModule, AuthModule, FileModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
