334ac0dbc95be363887f3ac5eefc7962178fbb65
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2026-01-22 17:47:53 -0300

    feat: Implement auth-module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWTSECRET,
+      signOptions: {
+        expiresIn: parseInt('1800'),
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
