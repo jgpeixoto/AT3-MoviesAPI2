c576abf3d83d2ac9b7f6244528b74415350400ec
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2026-01-22 17:47:53 -0300

    feat: Implement auth-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MoviesModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigService available globally
+      envFilePath: `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`,
+      cache: true,
+    }),
+    MoviesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
