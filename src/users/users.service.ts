3be1f540b4fa924e797787f07b8dbe8107ddcf28
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2026-01-26 19:45:41 -0300

    feat: Initial crud of users

---
+++ src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(user: User) {
+    return this.prisma.user.create({ data: user });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
