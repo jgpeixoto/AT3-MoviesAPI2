7a5825d87aef9a53e4a665d2bced085f42f4e97b
Stephane Bruna <stpbruna@gmail.com>
2026-01-26 15:58:05 -0300

    feat(ratings): implement full ratings resource and remove schema comment

---
+++ src/ratings/ratings.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { RatingsService } from './ratings.service';
+import { RatingsController } from './ratings.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [PrismaModule, AuthModule],
+  controllers: [RatingsController],
+  providers: [RatingsService],
+  exports: [RatingsService],
+})
+export class RatingsModule {}
