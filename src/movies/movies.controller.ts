af60d1815b8202c7ac54cd4791b8600e731c83e9
jgpeixoto <joaogabrielpeixoto2005@gmail.com>
2026-01-26 14:06:58 -0300

    feat(movies): implement AuthGuard on protected routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -10,16 +10,19 @@ import {
   UsePipes,
   Query,
   Patch,
+  UseGuards,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { FindMovieDto } from './dto/find-movie.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
+  @UseGuards(AuthGuard)
   @UsePipes(ValidationPipe)
   @Post()
   create(@Body() createMovieDto: CreateMovieDto) {
@@ -38,12 +41,14 @@ export class MoviesController {
     return this.moviesService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @UsePipes(ValidationPipe)
   @Put(':id')
   update(@Param('id') id: string, @Body() updateMovieDto: CreateMovieDto) {
     return this.moviesService.update(+id, updateMovieDto);
   }
 
+  @UseGuards(AuthGuard)
   @UsePipes(ValidationPipe)
   @Patch(':id')
   updatePartial(
@@ -53,6 +58,7 @@ export class MoviesController {
     return this.moviesService.updatePartial(+id, updateMovieDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.moviesService.remove(+id);
