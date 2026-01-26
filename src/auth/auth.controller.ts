801713d282476832475e93316117b6b38784d66c
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2026-01-26 15:56:36 -0300

    refactor: auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,13 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  UnauthorizedException,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly Service: AuthService) {}
   @Post('login')
-  login(@Body() body) {
-    if (body.user === 'luiz' && body.password === '123') {
-      //esse teste seria feito no banco
-      //auth ok
-      const id = 1; //esse id viria do banco de dados
-      const token = this.Service.createToken(id);
-      return { auth: true, token: token };
-    }
-    throw new UnauthorizedException('Erro ao fazer login');
-  }
-
-  @UseGuards(AuthGuard)
-  @Get('all')
-  getall() {
-    return 'Lanche';
+  async login(@Body() body) {
+    const email: string = body.email;
+    const password: string = body.password;
+    return this.Service.authenticateUser(email, password);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,40 +1,63 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 
-// interface JwtPayload {
-//   id: number;
-// }
 @Injectable()
 export class AuthService {
-  constructor(private readonly service: JwtService) {}
+  constructor(
+    private readonly service: JwtService,
+    private readonly prisma: PrismaService
+  ) {}
 
-  createToken(id: number): string {
-    return this.service.sign({ id }) as string;
+  async createToken(id: number, email: string): Promise<string> {
+    return this.service.signAsync({ id, email });
   }
 
-  checkToken(token: string) {
+  async checkToken(token: string) {
     try {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return this.service.verify(token.replace('Bearer ', ''));
+      return this.service.verifyAsync(token.replace('Bearer ', ''));
     } catch (err) {
       throw new UnauthorizedException(err);
     }
   }
 
-  EncryptPassword(password: string) {
+  async EncryptPassword(password: string) {
     try {
       return bcrypt.hash(password, 10);
     } catch (error) {
-      throw new Error(error);
+      const errorMessage: string = error.message;
+      throw new Error(errorMessage);
     }
   }
 
-  ComparePassword(password: string, userPassword: string): Promise<boolean> {
+  async ComparePassword(
+    password: string,
+    userPassword: string
+  ): Promise<boolean> {
     try {
       return bcrypt.compare(password, userPassword);
     } catch (error) {
       throw new UnauthorizedException(error);
     }
+  }
+
+  async authenticateUser(email: string, password: string) {
+    const user = await this.prisma.user.findUniqueOrThrow({ where: { email } });
+    if (!user) {
+      throw new NotFoundException(`user with EMAIL: ${email} not found`);
+    }
+
+    const userAproved = await this.ComparePassword(password, user.password);
+    if (!userAproved) {
+      throw new UnauthorizedException('invalid credentials');
+    }
+    const token = await this.createToken(user.id, user.email);
+    return { user: user, token: token };
   }
 }
