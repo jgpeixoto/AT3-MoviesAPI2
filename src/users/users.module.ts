3692c82b10d3cd80d31694158c0a0cae92912967
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2026-01-27 11:24:31 -0300

    feat: UsersMiddleware for request validation

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { UsersMiddleware } from './users.middleware';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersMiddleware],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UsersMiddleware)
+      .forRoutes(
+        { path: 'users', method: RequestMethod.POST },
+        { path: 'users', method: RequestMethod.PATCH }
+      );
+  }
+}
