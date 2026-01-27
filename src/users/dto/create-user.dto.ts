a6dbf663c73d707fefaccf4025343aed0243f8f4
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2026-01-27 14:32:18 -0300

    refactor: user validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+  @IsString()
+  @MinLength(6)
+  @IsNotEmpty()
+  password: string;
+}
