823c829f6a778a4ca945e6bae8a061d69864f140
Ryan <ryansantos03442@gmail.com>
2026-01-27 17:15:30 -0300

    feat: add movie list response dto

---
+++ src/personal-movie-list/dto/movie-list-response.dto.ts
@@ -0,0 +1,10 @@
+export class MovieListResponseDto {
+  id: number;
+  title: string;
+  description: string;
+  releaseYear: number;
+  genre: string;
+  duration: number;
+  avgRating: number;
+  totalRatings: number;
+}
